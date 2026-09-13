daca9d166950fb9482206a219533591adc660e0c
Tsjippy <ewaldha@hotmail.com>
2026-09-13 06:42:37 +0200

    skip:

--- tsjippy-html-email.php
+++ tsjippy-html-email.php
@@ -12,9 +12,9 @@ use TSJIPPY;
  * AuthorURI:            harmseninnigeria.nl
  * Requires at least:    6.3
  * Requires PHP:         8.3
- * Plugin URI:            https://github.com/Tsjippy/htmlemail/
+ * Plugin URI:           https://github.com/Tsjippy/htmlemail/
  * Tested:               7.1
- * TextDomain:            tsjippy
+ * TextDomain:           tsjippy
  * Requires Plugins:    
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
